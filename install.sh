ad8df870e2fcb18c1a565da9cf6677b9dc4c2a8f
antonybonn <antonybonn@outlook.com>
2024-06-10 02:51:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 sudo apt-get update -y && sudo apt-get install curl nginx -y
-
-wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-
-sudo bash install-release.sh
+wget --no-check-certificate https://raw.githubusercontent.com/antonybonn/v2ray/main/install-release.sh && chmod +x install-release.sh && sudo ./install-release.sh
 
 # JSON配置内容
-
 CONFIG='{
   "inbounds": [
     {
@@ -30,14 +26,11 @@ CONFIG='{
 }'
 
 # 创建目录（如果尚不存在）
-
 sudo mkdir -p /usr/local/etc/v2ray
-
 # 写入配置到文件
-
 echo "$CONFIG" | sudo tee /usr/local/etc/v2ray/config.json > /dev/null
 
 sudo systemctl enable v2ray
 sudo systemctl start v2ray
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && sudo ./bbr.sh
+wget --no-check-certificate https://raw.githubusercontent.com/antonybonn/v2ray/main/bbr.sh && chmod +x bbr.sh && sudo ./bbr.sh
 sudo systemctl restart v2ray
